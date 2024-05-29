19aca28f9d47c4f0c2045d59d302cfa82b43fb7e
James Tennant <jorahty@gmail.com>
2024-05-29 16:59:33 -0700

    fix category wrap

--- components/category/list.tsx
+++ components/category/list.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export default function CategoryList({ categories }: { categories: any[] }) {
   return (
-    <div className="flex gap-2">
+    <div className="flex gap-3 flex-wrap">
       {categories.map(({ slug, display_name }) => (
         <Button key={slug} variant="secondary" className="rounded-3xl" asChild>
           <Link href={`/category/${slug}`}>{display_name}</Link>
