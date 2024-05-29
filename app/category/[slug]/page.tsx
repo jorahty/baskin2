d62b89c09b3838485cc0117511de6b6745cec588
James Tennant <jorahty@gmail.com>
2024-05-29 16:53:24 -0700

    simplify

--- app/category/[slug]/page.tsx
+++ app/category/[slug]/page.tsx
@@ -3,21 +3,16 @@ import CategoryCrumbs from '@/components/category/crumbs';
 import CategoryList from '@/components/category/list';
 import ProductGrid from '@/components/product/grid';
 
-export default async function CategoryPage({ params }: { params: { category: string } }) {
+export default async function CategoryPage({ params: { slug } }: { params: { slug: string } }) {
   const supabase = createClient();
 
-  const ancestors = (await supabase.rpc('get_ancestors', { slug_value: params.category })).data!;
+  const ancestors = (await supabase.rpc('get_ancestors', { slug_value: slug })).data!;
 
   const children = (
-    await supabase
-      .from('categories')
-      .select('slug, display_name')
-      .eq('parent_slug', params.category)
+    await supabase.from('categories').select('slug, display_name').eq('parent_slug', slug)
   ).data!;
 
-  const products = (
-    await supabase.rpc('get_products_by_category', { category_slug: params.category })
-  ).data!;
+  const products = (await supabase.rpc('get_products_by_category', { category_slug: slug })).data!;
 
   return (
     <main className="flex flex-col gap-5 p-10">
