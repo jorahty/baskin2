4c80543c6ddc7c75fbc2bb407bb561d115c05d2f
James Tennant <jorahty@gmail.com>
2024-05-29 16:31:59 -0700

    adjust product card

--- components/product/grid.tsx
+++ components/product/grid.tsx
@@ -2,12 +2,12 @@ import Image from 'next/image';
 
 export default function ProductGrid({ products }: { products: any[] }) {
   return (
-    <div className="grid gap-5 grid-cols-1 min-[520px]:grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+    <div className="grid gap-x-5 gap-y-10 grid-cols-1 min-[520px]:grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
       {products.map(({ id, title, price, images }) => {
         const src = `https://smgaxvzzaljxhpqpejww.supabase.co/storage/v1/object/public/product_images/${images[0]}.jpeg`;
 
         return (
-          <div key={id} className="flex flex-col gap-2">
+          <div key={id} className="flex flex-col gap-3">
             <div className="rounded-xl overflow-hidden relative aspect-[8/9]">
               <Image
                 className="transition-all hover:scale-105 object-cover"
@@ -18,9 +18,9 @@ export default function ProductGrid({ products }: { products: any[] }) {
             </div>
 
             <div className="flex gap-4">
-              <div>[avatar]</div>
+              <div className="w-10 h-10 bg-secondary rounded-full" />
               <div>
-                <p>{price}</p>
+                <p className="font-semibold">${price.toLocaleString()}</p>
                 <p>{title}</p>
               </div>
             </div>
