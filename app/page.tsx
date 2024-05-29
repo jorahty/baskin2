03d84dbee0502f42e41541b6ca6c52ff3b1bda9a
James Tennant <jorahty@gmail.com>
2024-05-29 01:32:31 -0700

    list categories

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
-export default function Home() {
+import { Button } from '@/components/ui/button';
+import { createClient } from '@/supabase/server';
+
+export default async function Notes() {
+  const supabase = createClient();
+
+  const categories = (
+    await supabase.from('categories').select('slug, display_name').eq('parent_slug', 'all')
+  ).data!;
+
   return (
-    <main>
-      <p>hello world</p>
+    <main className="flex gap-2 p-2">
+      {categories.map((category) => (
+        <Button variant="secondary" className="rounded-3xl">
+          {category.display_name}
+        </Button>
+      ))}
     </main>
   );
 }
