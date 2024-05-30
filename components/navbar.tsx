10fc90f07feee4bc7eb34ed0acb8ed5941c54946
James Tennant <jorahty@gmail.com>
2024-05-29 17:16:42 -0700

    logo

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,9 @@
+import Logo from './logo';
+
 export default function Navbar() {
   return (
-    <section className="bg-secondary p-4 flex justify-between">
-      <h1>Baskin</h1>
+    <section className="bg-secondary p-4 flex justify-between items-center">
+      <Logo />
       <p>Search for products...</p>
       <p>[avatar]</p>
     </section>
