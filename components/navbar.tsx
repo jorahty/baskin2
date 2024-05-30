5fd69f8bfc4f6ff500f907c54f7f7b37d50523bb
James Tennant <jorahty@gmail.com>
2024-05-29 17:25:13 -0700

    make logo clickable

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,12 @@
+import Link from 'next/link';
 import Logo from './logo';
 
 export default function Navbar() {
   return (
-    <section className="p-4 flex justify-between items-center">
-      <Logo />
+    <section className="py-5 px-7 flex justify-between items-center">
+      <Link href="/">
+        <Logo />
+      </Link>
       <p>Search for products...</p>
       <p>[avatar]</p>
     </section>
