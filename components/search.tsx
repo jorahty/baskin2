fea8014b04177e02a43b18f675155c7c0dd683a1
James Tennant <jorahty@gmail.com>
2024-05-29 19:33:59 -0700

    fix search handler

--- components/search.tsx
+++ components/search.tsx
@@ -7,17 +7,13 @@ export default function Search() {
   function onSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
 
-    // TODO: Not implemented.
-    console.log('go! 🔥');
+    console.log((e.target as any).search.value);
   }
 
   return (
     <form onSubmit={onSubmit} className="relative max-w-lg w-80 lg:w-full">
-      <Input placeholder="Search for products..." autoComplete="off" />
-      <button
-        type="submit"
-        onClick={onSubmit as any}
-        className="absolute right-0 top-0 mr-3 flex h-full items-center">
+      <Input placeholder="Search for products..." autoComplete="off" type="text" name="search" />
+      <button type="submit" className="absolute right-0 top-0 mr-3 flex h-full items-center">
         <SearchIcon className="h-4" />
       </button>
     </form>
