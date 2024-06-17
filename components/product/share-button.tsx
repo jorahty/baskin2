a5201dd9c293d7b4d9d49340efadb47ec5445021
James Tennant <jorahty@gmail.com>
2024-06-17 12:07:52 -0700

    import dropdown menu for share button

--- components/product/share-button.tsx
+++ components/product/share-button.tsx
@@ -1,7 +1,30 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+
 import { Button } from '@/components/ui/button';
 import ShareIcon from '../share-icon';
 
 export default function ShareButton() {
+  // return (
+  //   <DropdownMenu>
+  //     <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+  //     <DropdownMenuContent>
+  //       <DropdownMenuLabel>My Account</DropdownMenuLabel>
+  //       <DropdownMenuSeparator />
+  //       <DropdownMenuItem>Profile</DropdownMenuItem>
+  //       <DropdownMenuItem>Billing</DropdownMenuItem>
+  //       <DropdownMenuItem>Team</DropdownMenuItem>
+  //       <DropdownMenuItem>Subscription</DropdownMenuItem>
+  //     </DropdownMenuContent>
+  //   </DropdownMenu>
+  // );
+
   return (
     <Button variant="secondary" className="grow font-semibold">
       <ShareIcon />
